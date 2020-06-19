6c2b4d80cb887d862e53cf4d55d9449bcb5d4f87
Connor Meads <meads.connoredward@gmail.com>
2020-06-19 13:33:29 -0600

    First successful conversion of infix to postfix.  Still needs a lot of fine tuning, but it officially is starting to work

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OrderOfOps
 {
@@ -8,12 +9,14 @@ namespace OrderOfOps
         {
             //variables
             string algExpression;
+            string postfixExpression;
             string errMessage = "";
             bool allGucci = true; 
             bool repeat = false;
-            char[] operands = {'*', '/', '+', '-', '%'};
+            char[] operators = {'*', '/', '+', '-', '%'};
             int numUnclosedParenthesis = 0;
             int numUnclosedBrackets = 0;
+            Stack<char> stack = new Stack<char>();
 
             //Intro
             Console.WriteLine("\n\nWelcome to OrderOfOps!\nThis program takes an algebraic expression that you type into the console, tests for valid input and outputs the result.");
@@ -28,7 +31,7 @@ namespace OrderOfOps
                 //Get rid of whitespaces and replace 'x' with '*'
                 algExpression = algExpression.Replace(" ", "").Replace("x", "*");
 
-                //Check if the first & last character are operands (with the exception of a negative sign at the beginning of the statement)
+                //Check if the first & last character are operators (with the exception of a negative sign at the beginning of the statement)
                 if(algExpression[0] == '+' || algExpression[algExpression.Length - 1] == '+'
                 || algExpression[0] == '*' || algExpression[algExpression.Length -1 ] == '*'
                 || algExpression[0] == '/' || algExpression[algExpression.Length - 1] == '/'
@@ -76,21 +79,21 @@ namespace OrderOfOps
                         }
                         
 
-                        //checks if there are two operands next to each other
-                        for(int j = 0; j < operands.Length; j++)
+                        //checks if there are two operators next to each other
+                        for(int j = 0; j < operators.Length; j++)
                         {
-                            if(algExpression[i] == operands[j] || algExpression[i] == '(' || algExpression[i] == '^') {
-                                for(int g = 0; g < operands.Length; g++)
+                            if(algExpression[i] == operators[j] || algExpression[i] == '(' || algExpression[i] == '^') {
+                                for(int g = 0; g < operators.Length; g++)
                                 {
                                     if(!allGucci)
                                     {
                                         break;
                                     }
-                                    if(algExpression[i+1] == operands[g]  && algExpression[i] != '('){
+                                    if(algExpression[i+1] == operators[g]  && algExpression[i] != '('){
                                         if(algExpression[i+1] == '-') {
                                             errMessage = errMessage + "\n*If you meant to indicate the number was negative, please put parenthesis around it*";
                                         }
-                                        errMessage = errMessage + "\nYou had two operands next to each other: " + algExpression[i] + algExpression[i+1];
+                                        errMessage = errMessage + "\nYou had two operators next to each other: " + algExpression[i] + algExpression[i+1];
                                         allGucci = false;
                                     }                                
                                 }
@@ -118,9 +121,55 @@ namespace OrderOfOps
                     repeat = false;
                 }
             } while(repeat);
+
+            //Solve the problem
+            //Convert from Infix to Postfix
+            postfixExpression = "";
+
+            for(int i = 0; i < algExpression.Length; i++)
+            {
+                if(Convert.ToInt32(algExpression[i]) >= 48 && Convert.ToInt32(algExpression[i]) <= 57) { //If the current character is 0-9, add it to the postfix expression
+                    postfixExpression += algExpression[i];
+                } else if(algExpression[i] == '(') { //push opening parenthesis
+                    stack.Push(algExpression[i]);
+                } else if(algExpression[i] == ')') { //some logic on closing parenthesis
+                    while(stack.Peek() != '(') {
+                        postfixExpression += stack.Pop();
+                    }
+                    stack.Pop();
+                } else { //logic for operators
+                    while(stack.Count > 0 && prescedence(algExpression[i]) < prescedence(stack.Peek()) && stack.Peek() != '(')
+                    {
+                        postfixExpression += stack.Pop();
+                    }
+                    stack.Push(algExpression[i]); //pushes the current operator onto the stack after popping the higher prescedented operators
+                }
+            }
+            while(stack.Count > 0)
+            {
+                postfixExpression += stack.Pop();
+            }
+            
+            Console.WriteLine("Infix: " + algExpression + "  ---> Postfix: " + postfixExpression);
         }
 
-        private static void errorMessage(string l_errMsg)
+        private static int prescedence(char l_char) //A function to test the prescedence (or weight) of an operator
+        {
+            switch(l_char)
+            {
+                case '+':
+                case '-':
+                    return 1; //heaviest
+                case '/':
+                case '*':
+                    return 2;
+                case '^':
+                    return 3; //lightest
+            }
+            return -1; //if, for whatever reason, the character isn't an operator
+        }
+
+        private static void errorMessage(string l_errMsg)  //A function to output an error, usually an error associated with incorrect input
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\n******************************************");
