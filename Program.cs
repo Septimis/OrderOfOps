49f4162128319c0116feb4c8fe2c9f3199c168bc
Connor Meads <meads.connoredward@gmail.com>
2020-06-19 17:08:11 -0600

    Successfully converting from infix --> postfix now.  Also made my infix expression a List<string> instead of a string to accomidate numbers >9.  Also made the postfix expression a List<string> object instead of a string for the same reason.  Next step is going to be evaluating my postfix expression

--- Program.cs
+++ Program.cs
@@ -9,14 +9,16 @@ namespace OrderOfOps
         {
             //variables
             string algExpression;
-            string postfixExpression;
+            List<string> infixExpression = new List<string>();
+            List<string> postfixExpression = new List<string>();
             string errMessage = "";
             bool allGucci = true; 
             bool repeat = false;
             char[] operators = {'*', '/', '+', '-', '%'};
             int numUnclosedParenthesis = 0;
             int numUnclosedBrackets = 0;
-            Stack<char> stack = new Stack<char>();
+            Stack<string> stack = new Stack<string>();
+            Stack<string> operatorStack = new Stack<string>();
 
             //Intro
             Console.WriteLine("\n\nWelcome to OrderOfOps!\nThis program takes an algebraic expression that you type into the console, tests for valid input and outputs the result.");
@@ -29,7 +31,7 @@ namespace OrderOfOps
 
                 //Logic to parse through the expression and check for valid input
                 //Get rid of whitespaces and replace 'x' with '*'
-                algExpression = algExpression.Replace(" ", "").Replace("x", "*");
+                algExpression = algExpression.Replace(" ", "").Replace("x", "*").Replace("[", "(").Replace("]", ")");
 
                 //Check if the first & last character are operators (with the exception of a negative sign at the beginning of the statement)
                 if(algExpression[0] == '+' || algExpression[algExpression.Length - 1] == '+'
@@ -58,15 +60,16 @@ namespace OrderOfOps
                             errMessage = errMessage + "\nCharacter " + algExpression[i] + " at index " + i + " was invalid...";
                         }
 
+                        //If the expression implies multiplication such as 3(4+2)
+                        if(i+1 < algExpression.Length && Convert.ToInt32(algExpression[i]) >= 48  && Convert.ToInt32(algExpression[i]) <= 57 && algExpression[i+1] == '(') {
+                            algExpression = algExpression.Insert(i+1, "*");
+                        }
+
                         //checks if there are equal number of open parenthesis as closing
                         if(algExpression[i] == '(') {
                             numUnclosedParenthesis++;
-                        } else if(algExpression[i] == '[') {
-                            numUnclosedBrackets++;
                         } else if(algExpression[i] == ')') {
                             numUnclosedParenthesis--;
-                        } else if(algExpression[i] == ']') {
-                            numUnclosedBrackets--;
                         }
 
                         //If, at any time, there are more closing parenthesis than opening, that's an error.
@@ -107,6 +110,26 @@ namespace OrderOfOps
                     }
                 }
 
+                string temp = "";
+                //convert the expression from a string to a list (to encorperate numbers >9)
+                for(int i = 0; i < algExpression.Length; i++)
+                {
+                    if(i+1 < algExpression.Length) { //to make sure we don't go out of bounds
+                        //this if statement checks to see if the current character is a number OR a . AND if the next character is a number OR a .
+                        if((Convert.ToInt32(algExpression[i]) >= 48 && Convert.ToInt32(algExpression[i]) <= 57 || Convert.ToInt32(algExpression[i]) == 46)
+                         && (Convert.ToInt32(algExpression[i+1]) >= 48 && Convert.ToInt32(algExpression[i+1]) <= 57 || Convert.ToInt32(algExpression[i+1]) == 46)) {
+                            temp += algExpression[i];
+                        } else { //This adds an item to the list if it's a single digit, an operator, or parenthesis
+                            temp += algExpression[i];
+                            infixExpression.Add(temp);
+                            temp = "";
+                        }
+                    } else {
+                        temp += algExpression[i];
+                        infixExpression.Add(temp);
+                    }
+                }
+
                 //Checks if there are any errors that occurred in the code and spits out the error
                 if(!allGucci) {
                     errorMessage(errMessage); //invoke the errorMessage function
@@ -124,46 +147,58 @@ namespace OrderOfOps
 
             //Solve the problem
             //Convert from Infix to Postfix
-            postfixExpression = "";
-
-            for(int i = 0; i < algExpression.Length; i++)
+            foreach(string i in infixExpression)
             {
-                if(Convert.ToInt32(algExpression[i]) >= 48 && Convert.ToInt32(algExpression[i]) <= 57) { //If the current character is 0-9, add it to the postfix expression
-                    postfixExpression += algExpression[i];
-                } else if(algExpression[i] == '(') { //push opening parenthesis
-                    stack.Push(algExpression[i]);
-                } else if(algExpression[i] == ')') { //some logic on closing parenthesis
-                    while(stack.Peek() != '(') {
-                        postfixExpression += stack.Pop();
+                if(Convert.ToInt32(i[0]) >= 48 && Convert.ToInt32(i[0]) <= 57) { //If the current character is 0-9, add it to the postfix expression
+                    postfixExpression.Add(i);
+                } else if(i == "(") { //push opening parenthesis
+                    stack.Push(i);
+                } else if(i == ")") { //some logic on closing parenthesis
+                    while(stack.Peek() != "(") {
+                        postfixExpression.Add(stack.Pop());
                     }
                     stack.Pop();
                 } else { //logic for operators
-                    while(stack.Count > 0 && prescedence(algExpression[i]) < prescedence(stack.Peek()) && stack.Peek() != '(')
+                    while(stack.Count > 0 && prescedence(i) <= prescedence(stack.Peek()) && stack.Peek() != "(")
                     {
-                        postfixExpression += stack.Pop();
+                        postfixExpression.Add(stack.Pop());
                     }
-                    stack.Push(algExpression[i]); //pushes the current operator onto the stack after popping the higher prescedented operators
+                    stack.Push(i); //pushes the current operator onto the convertStackafter popping the higher prescedented operators
                 }
             }
             while(stack.Count > 0)
             {
-                postfixExpression += stack.Pop();
+                postfixExpression.Add(stack.Pop());
             }
             
-            Console.WriteLine("Infix: " + algExpression + "  ---> Postfix: " + postfixExpression);
+            Console.Write("\nInfix: " + algExpression + "  ---> Postfix: ");
+            foreach(string i in postfixExpression) 
+            {
+                Console.Write(i + " ");
+            }
+
+            //Solve the postfix expression
+            foreach(string i in postfixExpression)
+            {
+                if(Convert.ToInt32(i[0]) >= 48 && Convert.ToInt32(i[0]) <= 57) { //if the current character is 0-9, add it to the stack containing operands
+                    stack.Push(i);
+                } else { //if the current character is an operator, pop operands from the stack.  Evaluate and push the result back to the stack
+                    
+                }
+            }
         }
 
-        private static int prescedence(char l_char) //A function to test the prescedence (or weight) of an operator
+        private static int prescedence(string l_char) //A function to test the prescedence (or weight) of an operator
         {
             switch(l_char)
             {
-                case '+':
-                case '-':
+                case "+":
+                case "-":
                     return 1; //heaviest
-                case '/':
-                case '*':
+                case "/":
+                case "*":
                     return 2;
-                case '^':
+                case "^":
                     return 3; //lightest
             }
             return -1; //if, for whatever reason, the character isn't an operator
